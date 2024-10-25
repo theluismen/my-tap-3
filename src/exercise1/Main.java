7e679323e4f515e43df34f45b834eca8b7e632a0
Luis Manuel Ocampo <luismanuel.ocampo@estudiants.urv.cat>
2024-10-25 11:25:37 +0200

    comentario Exercise 1

--- src/exercise1/Main.java
+++ src/exercise1/Main.java
@@ -2,6 +2,7 @@ package exercise1;
 
 public class Main {
 
+	/* 1. Convert the adapter sample to a Class Adapter. */
 	public static void main(String[] args) {
 		//CigaretteLighterReceptacle carLighter = new CigaretteLighterReceptacle();
 		//MP3Player myPlayer = new MP3Player("SanDisk",4,new UsbToCarAdapter(carLighter));
